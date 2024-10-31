c0011f80b5cc078484a5f3df947a87551df90284
Yuri Bessa <yuri14.levita@gmail.com>
2024-10-31 15:40:09 -0300

    Comentando Código em Python

--- aula_1.py
+++ aula_1.py
@@ -1,2 +1,16 @@
+#Primeira forma de comentar códigos.
+
+'''
+Segunda forma de comentar códigos.
+Autor: Yuri
+Data:31/10/24 
+Versão 1.0
+'''
+
+
+
 # Printando a tela
 print("Hello World")
+print("Printando de novo")
+
+
