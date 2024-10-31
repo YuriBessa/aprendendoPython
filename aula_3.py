cd8a9534dbd4f24c4ca9c62b3bb0064e8c52128f
Yuri Bessa <yuri14.levita@gmail.com>
2024-10-31 16:10:18 -0300

    Modificação de dados e tipagem de variável.

---
+++ aula_3.py
@@ -0,0 +1,10 @@
+# Modificação de dados
+
+#Python permite a tipagem da variável.
+x = str (3)
+y = int(4)
+z = float(5)
+
+print(x + x) # 33 (Contatenou as strings)
+print(y + y) # 8 (somou os inteiros)
+print(z + z) # 10,0 (Somou os floats)
