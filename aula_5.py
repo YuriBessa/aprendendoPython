1bb1daaa618e622480f3cce90d202f5e622ae88e
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-06 14:42:11 -0300

    Entrada de dados (Input)

---
+++ aula_5.py
@@ -0,0 +1,7 @@
+# Utilizando inputs
+
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+cidade = input('Qual cidade você mora: ')
+
+print('O ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' + cidade)
