5f1b67fad5a46b613e473ccc9e9003e27525703b
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-06 17:48:27 -0300

    Operações matemáticas

---
+++ aula_10.py
@@ -0,0 +1,18 @@
+#Operações matemáticas - O Python segue a precedência da matemática. 
+# 1 - Parenteses, 
+# 2 - Exponenciação,
+# 3 - Multilplica ou Divisão (Tem a mesma precedencia, é executado da esquerda para a direita)
+# 4 - Adição e Subtração (Tem a mesma precedencia é executado da esquerda para a direita)
+
+
+calculo = 2 + 2 # 4 
+calculo2 = 2 + 2 * 3 # 8  
+calculo3 = (2 + 2) * 3 # 12 
+calculo4 = 2 + 2 * 3 / 2 # 5.0
+calculo5 = 2 ** 3 + 4 # 12 
+
+print (calculo)
+print (calculo2)
+print (calculo3)
+print (calculo4)
+print (calculo5)
