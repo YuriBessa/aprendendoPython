81d615bab9efd79e23a9b17a4eac8b5694818bc8
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-06 16:10:14 -0300

    Aprendendo Slicing

---
+++ aula_7.py
@@ -0,0 +1,16 @@
+
+#Aprendendo Slicing
+
+fruta ='Abacate'
+#index= 0123456
+
+print(fruta[2:6]) # -> acat
+print(fruta[-1]) # -> e ~ Conta de trás opra frente.
+ 
+valor = 99.75
+#index= 01234
+valor = str(valor) # Tem que converter variável em string pra não dar erro.
+
+print(valor[3:4]) # 7 
+print(valor[:2]) # 99
+print(valor[2:]) # .75
