fd5618db77e2bfd551883be4ffda287b3d2bdb47
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-07 15:00:49 -0300

    Operadores de atribuição

---
+++ aula_12.py
@@ -0,0 +1,12 @@
+# Operadores de atribuição
+
+x = 10
+# x = x + 5 // 15
+# x += 5  // 15 - É uma forma mais resumida de escrever a operação acima
+# x -= 5  // 5 
+# x *= 5  // 50 
+# x /= 5  // 2.0
+# x %= 4  // 2 - Retorna o resto de x (10)/4 que é 2.
+ 
+
+print (x) 
