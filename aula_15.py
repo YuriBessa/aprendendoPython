004c7eba0fa64f128efb3bf2458607a26a057afc
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-07 15:43:12 -0300

    Multiplos operadores de comparação

---
+++ aula_15.py
@@ -0,0 +1,17 @@
+# Multiplos operadores de comparação
+
+valor = 10
+
+if valor >= 20 and valor < 40: 
+    print('O valor foi aceito')
+else:
+    print('O valor não foi aceito')
+
+#forma diferente de escrever a mesma condicional:
+
+valor = 30
+
+if 20 <= valor < 40: 
+    print('O valor foi aceito')
+else:
+    print('O valor não foi aceito')
