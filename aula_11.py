c890adfb74584a6e40c8df488ee3cfae3f3f41f7
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-07 14:49:46 -0300

    Operadores de comparação.

---
+++ aula_11.py
@@ -0,0 +1,27 @@
+# Operadores de comparação.
+
+# == Igual 
+# != Diferente de 
+# >  Maior que
+# <  Menor que
+# >= Maior igual a
+# <= Menor igual a
+ 
+operadores = 10 == 10 # True - Se a sentença for verdadeira retorna true se não for, retorna false
+operadores2 = 10 == 11 # False 
+operadores3 = 'banana' == 'Banana' # False (Case Sensitive) 
+operadores4 = 'banana' != 'Banana' # true (Case Sensitive)
+operadores5 = 10 < 9 # False  
+operadores6 = 10 > 10 # False
+operadores7 = 10 <= 100 # True
+operadores8 = 10 >= 100 # False
+
+
+print(operadores)
+print(operadores2)
+print(operadores3)
+print(operadores4)
+print(operadores5)
+print(operadores6)
+print(operadores7)
+print(operadores8)
