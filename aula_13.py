4d364316f98a430340fddb3ac50c9586cd8b01fd
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-07 15:08:37 -0300

    Operadores Condicionais

---
+++ aula_13.py
@@ -0,0 +1,11 @@
+# Operadores Condicionais
+
+velocidade = 50
+
+if velocidade > 110:
+    print('Acima velocidade Permitida')
+    print('Favor reduzir a sua velocidade')
+elif velocidade < 60:
+    print('Favor dirigir acima de 80 Km/h')
+else:
+    print('Velocidade Ok!')
