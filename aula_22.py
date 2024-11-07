92dbd3991b077227252feacdf31d6f237cece818
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-07 17:27:30 -0300

    Loop While

---
+++ aula_22.py
@@ -0,0 +1,13 @@
+# Loop While
+
+#Excelente para loops que dependem de uma condição.
+
+# Criar uma promoção para um produto num valor de R$ 100,00
+
+valor = 100
+dia = 1
+
+while valor > 20: 
+    print (f' No dia {dia} o produto vai ser vendido por R$ {valor}')
+    valor -= 5
+    dia += 1
