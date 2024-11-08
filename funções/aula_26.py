4397da9ee43472b89a41ddf94c1bb10ddfc864f7
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-08 14:45:55 -0300

    Funções (Escopo)

---
+++ funções/aula_26.py
@@ -0,0 +1,12 @@
+# Funções (Escopo)
+
+def soma_dois_numero():
+    numero1 = 10
+    numero2 = 5
+    resultado = numero1 + numero2
+    print(resultado)
+
+
+soma_dois_numero()
+
+# print(resultado) // mostra um  erro pois a variavel resultado está somente dentro do escopo da função
