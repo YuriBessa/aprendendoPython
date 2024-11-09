759ef745a2bd5f4143a866a561b53666ac24709b
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-08 22:15:16 -0300

    Objetivo das funções.

---
+++ funções/aula_29.py
@@ -0,0 +1,12 @@
+# Funções 
+# Realizam uma tarefa ou,
+# Calcula e retorna um valor.
+
+def cliente1(nome): # Executando tarefa
+    print(f'Olá {nome}')
+
+def cliente2(nome): # retornando um valor.
+    return f'Olá {nome}' 
+
+cliente1("Maria")
+cliente2("José")
