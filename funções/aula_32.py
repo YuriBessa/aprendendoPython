4064f93574d6b36697377cace1cd8ab90962db28
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-09 13:30:19 -0300

    Importando um módulo

---
+++ funções/aula_32.py
@@ -0,0 +1,11 @@
+# Importando um módulo
+import math
+
+# calcular Fatorial
+
+fatorial = 4 * 3 * 2 * 1  # calculo manual
+print(fatorial)
+
+
+
+print(math.factorial(4)) # calculo por função específica.
