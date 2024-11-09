db91d6ecd4085b207f29cac889a7a5373beaf9f7
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-09 15:19:11 -0300

    Manipulando Listas

---
+++ Estrutura de Dados/aula_34.py
@@ -0,0 +1,11 @@
+# Manipulando Listas -
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiânia']
+numeros = [2,4,6]
+
+print(cidades[0]) # Rio de Janeiro
+print(cidades[1]) # São Paulo
+print(cidades[2]) # Salvador
+print(cidades[-1]) # Goiania - Quando colocamos um index negativo ele começa a contar de trás pra frente
+print(cidades[-4])
+
