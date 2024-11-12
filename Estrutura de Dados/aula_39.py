1eeed960a2eeed54d29d4756fb9782e17c1ae8fe
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-12 15:48:35 -0300

    Verificando itens em uma lista

---
+++ Estrutura de Dados/aula_39.py
@@ -0,0 +1,10 @@
+# Listas - Verificando itens em uma lista
+
+cor_cliente = input('Digite a cor desejada: ')
+
+cores = ['amarelo', 'verde', 'azul', 'vermelho']
+
+if cor_cliente.lower() in cores:
+    print('Em estoque')
+else:
+    print('Não temos essa cor em estoque')
