583a4f61a36c753506312d53a0bb1d19a554a3bd
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-12 15:41:04 -0300

    Looping dentro de uma lista

---
+++ Estrutura de Dados/aula_38.py
@@ -0,0 +1,13 @@
+# Listas - Looping dentro de uma lista
+
+valores = [50,80,110,150,170]
+
+for x in valores:
+    print(f'O valor final do produto é de R$ {x}')
+
+# Respostas
+# O valor final do produto é de R$ 50
+# O valor final do produto é de R$ 80 
+# O valor final do produto é de R$ 110
+# O valor final do produto é de R$ 150
+# O valor final do produto é de R$ 170
