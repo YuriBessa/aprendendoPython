df8c7b16643ee5b27fcd9c291b48492511194750
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-14 16:47:23 -0300

    Introdução a Dicionários

---
+++ Estrutura de Dados/aula_47.py
@@ -0,0 +1,8 @@
+# Introdução a Dicionários
+# Utiliza index no formato chave e valor
+# Aceita String, interger, float, booleam
+
+aluno = {'nome': 'Ana', 'idade': 16, 'nota_final': 'A', 'aprovação': True}
+
+print(aluno) # Imprime dicionário completo
+print(aluno['nome']) # Imprime o valor contido na Key 'nome'. 
