e3d4137c224c32d9f71b604cc2483dfc8315fc16
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-18 14:20:58 -0300

    Função Map com Lambda

---
+++ Estrutura de Dados/aula_54.py
@@ -0,0 +1,10 @@
+# Função Map com Lambda
+
+lista1 = [1,2,3,4]
+
+# def multi(x):
+#     return x*2
+
+# lista2 = map(lambda x: x * 2 , lista1)
+
+print(list(map(lambda x: x * 2 , lista1))) # [2, 4, 6, 8] - Todo o código foi reduzido para 2 linhas.
