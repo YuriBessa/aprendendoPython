4468c8560dc87fdaf7e4761a9a7b69318ebb3912
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-19 16:05:50 -0300

    POO - Conhecendo Classes

---
+++ Prog, Orientada a Objetos/aula_62.py
@@ -0,0 +1,14 @@
+# POO - Conhecendo Classes
+
+# Classes
+    # Utilizada para criar Objetos (intancias)
+    # Classes são utilizadas para agrupar dados e funções, podendo ser reutilizadas.
+    # Class: Frutas
+    # Objects: Abacate, Banana
+
+# Estamos trabalhando com OOP desde o início
+
+nome = 'Yuri'
+print(type(nome)) # <class 'str'> - é uma classe
+nome_minusculo = nome.lower() # o lower é um dos métodos (funcionalidade) da classe. 
+print(nome_minusculo) # yuri
