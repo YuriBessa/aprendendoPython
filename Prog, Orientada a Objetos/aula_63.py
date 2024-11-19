6f840a514f8a181abc7ffa6b7cb46bdf87592ba6
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-19 16:25:34 -0300

    Criando a sua primeira classe

---
+++ Prog, Orientada a Objetos/aula_63.py
@@ -0,0 +1,14 @@
+# Criando a sua primeira classe
+
+class Funcionarios: 
+    nome = 'Elena'
+    sobrenome = 'Cabral'
+    data_nascimento = '12/01/2009'
+
+usuario1 = Funcionarios()
+
+print(usuario1.nome) # Elena
+print(usuario1.sobrenome) # Cabral
+print(usuario1.data_nascimento) # 12/01/2009
+
+
