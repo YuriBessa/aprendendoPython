d14f224d51131d1d6ba75346d371a45aa308326a
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-21 14:24:21 -0300

    Criando e Importando Packages

---
+++ Módulos/aula_70.py
@@ -0,0 +1,8 @@
+# Criando e Importando Packages
+
+from aula_68_modulo import somar, multi
+from package_py.cadastro import cliente
+
+somar() # Esta função serve para somar valores
+multi() # Esta função serve para multiplicar valores
+cliente() # Cadastro do cliente
