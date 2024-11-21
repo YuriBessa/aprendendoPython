f31594b0e59a0af2ebb6703880418181f526e0b4
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-21 15:08:11 -0300

    Aplicando um Modulo

---
+++ Módulos/aula_71.py
@@ -0,0 +1,8 @@
+# Aplicando um Modulo
+
+from modulo_aula_71 import find_index
+
+list1 = ["a","b","c","d"]
+
+var1 = find_index(list1, 'b')
+print(var1) # 1
---
+++ Módulos/modulo_aula_71.py
@@ -0,0 +1,7 @@
+# Aplicando um Modulo
+
+def find_index(to_find, item):
+    for i, valor in enumerate(to_find):
+        if valor == item:
+            return i
+    return -1
