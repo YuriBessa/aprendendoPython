67bfd66227f127d9eba3c1731f006805a6fc7382
Yuri Bessa <yuri14.levita@gmail.com>
2024-11-21 14:09:38 -0300

    Criando e importando seu primeiro Modulo

---
+++ Módulos/aula_68.py
@@ -0,0 +1,15 @@
+#  Criando e importando seu primeiro Modulo
+
+# **** Primeira forma de importar ****
+
+# import aula_68_modulo
+
+# aula_68_modulo.somar() # Esta função serve para somar valores
+# aula_68_modulo.multi() # Esta função serve para multiplicar valores
+
+# **** Segunda forma ****
+from aula_68_modulo import somar, multi
+
+somar() # Esta função serve para somar valores
+multi() # Esta função serve para multiplicar valores
+
---
+++ Módulos/aula_68_modulo.py
@@ -0,0 +1,7 @@
+# Modulo
+
+def somar ():
+    print('Esta função serve para somar valores')
+
+def multi():
+    print('Esta função serve para multiplicar valores')
